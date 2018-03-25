cec22da70e8befccd146de2c99ececacbab27a09
Daniel Lopes <daniel@trilogo.com.br>
2018-03-25 11:37:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var botao = document.querySelector('.btn');
+
+botao.addEventListener("click", function () {
+    var ipn = document.querySelector('#campo-texto');
+    var divResultado = document.querySelector('#divResultado');
+
+    divResultado.innerHTML = ipn.value + "<br />" +  divResultado.innerHTML;
+    ipn.value = '';
+    // alert(ipn.value);
+
+});
+
